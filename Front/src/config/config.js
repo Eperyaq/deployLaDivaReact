463d3b7353f08bebd0f9d9e441387a641833629c
Elia <eliaserdev21@gmail.com>
2026-03-22 16:32:07 +0100

    d

--- Front/src/config/config.js
+++ Front/src/config/config.js
@@ -1,4 +1,6 @@
-//const API_URL = "https://back-production-15f1.up.railway.app/api"; // Cambiar esto cuando suba a producción
 
-const API_URL = "http://localhost:4000"; // Cambiar esto cuando suba a producción
+// Cambiar esto cuando suba a producción
+const API_URL = "https://ladivareact-production.up.railway.app";
+
+//const API_URL = "http://localhost:4000"; // Cambiar esto cuando suba a producción
 export default API_URL;
